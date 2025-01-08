c3f62eb567d5d645562a1cb806a0ddd363473688
yuunadesigner <brunalemezzz@gmail.com>
2025-01-08 16:07:59 -0300

    update script do ex011

--- aula12/ex011/script.js
+++ aula12/ex011/script.js
@@ -1,20 +1,20 @@
 function carregar() {
 var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
+var foto = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas`
 if (hora >= 0 && hora < 12) {
     //bom dia!!
-    img.src = 'fotomanha.png'
+    foto.src = 'fotomanha.png'
     document.body.style.background = '#ffd047'
 } else if (hora >= 12 && hora <= 18) {
     //boa tarde!!
-    img.src = 'fototarde.png'
+    foto.src = 'fototarde.png'
     document.body.style.background = '#ff8600'
 } else {
     //boa noite!!
-    img.src = 'fotonoite.png'
+    foto.src = 'fotonoite.png'
     document.body.style.background = '#007fba'
 }
 }
