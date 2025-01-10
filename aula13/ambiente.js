d45803bf37bd9bd7a596be8b9a4aef29353b73f0
yuunadesigner <brunalemezzz@gmail.com>
2025-01-10 11:45:46 -0300

    Criei ambiente js da aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+var c = 1 
+for (var c=1; c<=10; c++) {
+    console.log('tudo bem!')
+}
+
+
+/*
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 9)
+ */
+
+
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('vai começar...')
+for(var c = 1;c <= 4; c++) {
+    0console.log(c)
+}
+console.log('fim')
